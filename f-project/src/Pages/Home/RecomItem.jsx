4715baa7d2008f4a05db7b03dbf22aff3b8654b6
Tina Georgescu <cristina.badea30@yahoo.com>
2021-11-17 17:11:59 +0000

    Adding style to RecomItem
    Adding style to RecomItem component

--- f-project/src/Pages/Home/RecomItem.jsx
+++ f-project/src/Pages/Home/RecomItem.jsx
@@ -4,18 +4,22 @@ import "./RecomItem.css";
 const RecomItem = (props) => {
     console.log(props);
     return (
-        <div className='recom-container'>
-            <h1> Title</h1>
-            <img src={props.itemImage}/>
-            <div>
-                <p>{props.recom1}</p>
-                <p>{props.recom2}</p>
-                <p>{props.recom3}</p>
+        <div>  
+            <div className='recom-row'>
+                <div className="recom-col recom-img">
+                    <img className = 'img1'src={props.itemImage} />   
+                </div>
+                <div className='recom-col recom-col-background'>
+                    <h1> Our recommendatios:</h1>
+                    <p>{props.recom1}</p>
+                    <p>{props.recom2}</p>
+                    <p>{props.recom3}</p>
+                </div>
             </div>
-            <div>
-                <button>Add to cart</button>
-            </div>
-        </div>);
+
+        <button className="recom-btn"> Add to cart</button>
+
+        </div> );
 }
 
 export default RecomItem;
